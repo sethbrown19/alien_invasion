0b897e85d72420c0c89302a95559af7c9bcdee84
Seth Alexander Brown <sethbrown19@outlook.com>
2020-10-28 18:59:50 -0400

    set background color of screen for game to a light grey

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,9 @@ class AlienInvasion:
     
         self.screen = pygame.display.set_mode((1200, 800)) 
         pygame.display.set_caption("Alien Ivasion")
+        
+        # set back ground color
+        self.bg_color = (230,230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -18,6 +21,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
         
             #make the most resnetly drawn screen visible
             pygame.display.flip()
@@ -25,3 +30,4 @@ class AlienInvasion:
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
+    
