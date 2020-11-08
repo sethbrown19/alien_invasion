38b817c5a64a7f8909e79a55ebe93a630810b45f
Seth Alexander Brown <sethbrown19@outlook.com>
2020-11-08 17:03:50 -0500

    refractored for better runtime. Added class _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,10 +21,14 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
+            self._check_events()
+            
+    def _check_events():
+        """Respond to key presses and mouse events"""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    
             # redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
