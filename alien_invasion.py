db3570433d1b876760234d38534c238c007c0ec4
Seth Alexander Brown <sethbrown19@outlook.com>
2020-11-15 13:10:56 -0500

    limit on bullets allowed to be on screen at one time

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,13 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            
+            # get rid of bullets that have dissappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                print(len(self.bullets))
+                
             self._update_screen()
             
     def _check_events(self):
@@ -62,8 +69,9 @@ class AlienInvasion:
     
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
                     
     def _update_screen(self):
         """Update images on screen and flip to the new screen"""
