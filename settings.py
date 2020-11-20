634eca1638292599658a3bfe8839173b53913ada
Seth Alexander Brown <sethbrown19@outlook.com>
2020-11-20 18:25:31 -0500

    chagned speed and added fleets to the game once one fleet is destroyed

--- settings.py
+++ settings.py
@@ -10,14 +10,14 @@ class Settings:
         self.bg_color = (230,230,230)
         
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 1.6
         
         # Bullet Settings
-        self.bullet_speed = 1.2
+        self.bullet_speed = 1.5
         self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_height = 17
         self.bullet_color = (60,60,60)
-        self.bullets_allowed = 8
+        self.bullets_allowed = 7
         
         # Alien settings
         self.alien_speed = 1.0
