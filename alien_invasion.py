da3bff87ca4bba26f6cf812a42682ff976af72d6
Seth Alexander Brown <sethbrown19@outlook.com>
2020-11-21 19:39:28 -0500

    add points for aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,6 +73,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self. stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
             
             # Get rid of remaining aliens and bullets
             self.aliens.empty()
@@ -126,6 +127,10 @@ class AlienInvasion:
         """Respond to bullet and alien collisons"""
         # Remove any bullets and aliens that have collided
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
         
         if not self.aliens:
             # Destroy existing bullets and create new fleet
