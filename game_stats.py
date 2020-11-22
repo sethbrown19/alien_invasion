e84efc329c7a089a40d7f7b6ed7b7e376c9170f6
Seth Alexander Brown <sethbrown19@outlook.com>
2020-11-21 20:14:22 -0500

    added high score image.

--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,9 @@ class GameStats:
         # Start alien Invasions in an active state
         self.game_active = False
         
+        # High score should never be reset
+        self.high_score = 0
+        
     def reset_stats(self):
         """Initialize statistics that can chae during the game."""
         self.ships_left = self.settings.ship_limit
--- scoreboard.py
+++ scoreboard.py
@@ -14,8 +14,10 @@ class ScoreBoard:
         self.text_color = (30,30,30)
         self.font = pygame.font.SysFont(None, 48)
         
-        # Preparethe inital score image.
+        # Prepare the inital score image.
         self.prep_score()
+        self.prep_high_score()
+        
         
     def prep_score(self):
         """Turn the score into a rendered image"""
@@ -31,4 +33,24 @@ class ScoreBoard:
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image,self.score_rect)
+        self.screen.blit(self.high_score_image,self.high_score_rect)
+        
+    def prep_high_score(self):
+        """Turn the high score into a rendered image"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        
+        # Display the score baord at the top center of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.high_score_rect.top
+        
+    def check_high_score(self):
+        """Check to see if there is a new high score."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+        
+        
         
